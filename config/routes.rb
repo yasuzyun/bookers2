b09db5f17ded49dd15606a1072bc9f90884723a7
EC2 Default User <yasuzyun@yahoo.co.jp>
2022-12-27 04:12:25 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
  
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
-
+  get '/search', to: 'searches#search'
+ 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
   resources :book_comments, only: [:create, :destroy]
   resource :favorites, only: [:create, :destroy]
@@ -14,4 +15,6 @@ Rails.application.routes.draw do
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
+  
 end
