414afce8363b4c112a31797ab9b6f8beeef0d258
EC2 Default User <yasuzyun@yahoo.co.jp>
2022-12-31 02:43:25 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  
  
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
   resources :chats, only: [:show, :create]
 end
