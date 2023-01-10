29c921aee771b5c5b79d099778da21ddad35f4d3
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-01-10 12:31:55 +0000

    Initial Commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,16 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-   has_many :view_counts, dependent: :destroy
+  has_many :view_counts, dependent: :destroy
   has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
   
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } # 前週
   
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
