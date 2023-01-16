dc3e33f29d0b3f8241f443abf30a1bf6eae02312
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-01-16 10:00:45 +0000

    Initial Commit

--- app/models/groups_user.rb
+++ app/models/groups_user.rb
@@ -1,4 +1,4 @@
-class GroupUser < ApplicationRecord
+class GroupsUser < ApplicationRecord
   belongs_to :group
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_12_214418) do
+ActiveRecord::Schema.define(version: 2023_01_16_095846) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -67,6 +67,13 @@ ActiveRecord::Schema.define(version: 2023_01_12_214418) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "group_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
