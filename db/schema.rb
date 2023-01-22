1379f50a20f4887805f431fc4eedff43904fc915
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-01-22 03:50:04 +0000

    Initial Commit

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2023_01_19_105047) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.float "rate"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
